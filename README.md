72e42150c4cd6b0c16d4c0356aa394e5f90c6c0e
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2021-11-29 16:53:53 +0200

    feat(local): initial compose setup for local dev env

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 ## opendata-drupal ![GitHub workflow](https://github.com/vrk-kpa/opendata-drupal/actions/workflows/create-release.yml/badge.svg)
 
 Drupal docker image for open data portal (avoindata.fi). 
+
+### Local development
+
+Local development environment is built using docker-compose. The files are in the ´local´ folder and the compose file is setup so that only `drupal` image is built, other images are pulled with `latest` tags.
+
+#### Build & run
+
+```bash
+# build frontend (in modules/ytp-assets-common/)
+npm install && gulp
+# bring docker services up (in local/)
+docker-compose -p opendata-drupal up --build -d
+```
+
+#### Live editing
+
+The following folders can be edited live, they are mounted directly from host.
+
+* modules/avoindata-*
+* modules/ytp-assets-common/resources
--- scripts/init_filesystems.sh
+++ scripts/init_filesystems.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "init_filesystems() ..."
-
-# TODO: check if migration fs is mounted, migrate data if it is
-
-# init mounted filesystems (ECS Fargate EFS limitation forces this approach)
-echo "initializing ${SITE_DIR}, ${THEME_DIR}, ${CORE_DIR} & ${WWW_DIR}/resources ..."
-rsync -au ${BASE_DIR}/sites/ ${SITE_DIR} &
-rsync -au ${BASE_DIR}/themes/ ${THEME_DIR} &
-rsync -au ${BASE_DIR}/core/ ${CORE_DIR} &
-rsync -au --delete ${BASE_DIR}/resources/ ${WWW_DIR}/resources &
-wait
+if [[ "${DEV_MODE}" == "true" ]]; then
+    # don't do anything because folders are mounted from host
+    echo "init_filesystems() ... skipping because DEV_MODE == ${DEV_MODE} ..."
+else
+    # init mounted filesystems (ECS Fargate EFS limitation forces this approach)
+    echo "init_filesystems() initializing ${SITE_DIR}, ${THEME_DIR}, ${CORE_DIR} & ${WWW_DIR}/resources ..."
+    rsync -au ${BASE_DIR}/sites/ ${SITE_DIR} &
+    rsync -au ${BASE_DIR}/themes/ ${THEME_DIR} &
+    rsync -au ${BASE_DIR}/core/ ${CORE_DIR} &
+    rsync -au --delete ${BASE_DIR}/resources/ ${WWW_DIR}/resources &
+    wait
+fi
