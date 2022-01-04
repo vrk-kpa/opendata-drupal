e5cf230dcbbadef8efcb7978d511386bdfcfd3df
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2022-01-04 11:08:11 +0200

    chore: fix remaining issues with local dev env

--- drupal/scripts/init_filesystems.sh
+++ drupal/scripts/init_filesystems.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [[ "${DEV_MODE}" == "true" ]]; then
-    # don't do anything because folders are mounted from host
-    echo "init_filesystems - skipping because DEV_MODE = 'true' ..."
+  # init mounted filesystems
+  echo "init_filesystems - DEV_MODE = 'true', initializing '${WWW_DIR}/resources' ..."
+  rsync -au --delete ${MOD_DIR}/ytp-assets-common/resources/ ${WWW_DIR}/resources
 else
   # migrate data if migration fs is mounted to known path
   # NOTE: this is a bad idea, because EFS->EFS rsync perf is so bad!
