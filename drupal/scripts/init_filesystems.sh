0613b0995409e79365af4d227bfc530f70179ca0
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2022-01-20 16:42:34 +0200

    fix: remove EFS migration procedure from init_filesystems.sh

--- drupal/scripts/init_filesystems.sh
+++ drupal/scripts/init_filesystems.sh
@@ -8,19 +8,6 @@ if [[ "${DEV_MODE}" == "true" ]]; then
   cp -rf ${WWW_DIR}/resources/vendor/@fortawesome/fontawesome/webfonts/.  ${THEME_DIR}/avoindata/fonts && \
   cp -rf ${WWW_DIR}/resources/vendor/bootstrap/dist/fonts/.               ${THEME_DIR}/avoindata/fonts
 else
-  # migrate data if migration fs is mounted to known path
-  # NOTE: this is a bad idea, because EFS->EFS rsync perf is so bad!
-  #       it's left here as an example, can be deleted later...
-  if [[ -d "/mnt/ytp_files/drupal" ]]; then
-    echo "init_filesystems - migrating data from '/mnt/ytp_files/drupal' to '${SITE_DIR}/default/files' ..."
-    mkdir -p ${SITE_DIR}/default/files && chown -R www-data:www-data ${SITE_DIR}/default
-    rsync -au --chown=www-data:www-data /mnt/ytp_files/drupal/ ${SITE_DIR}/default/files
-    echo "printing migration source contents:"
-    ls -lah /mnt/ytp_files/drupal
-    echo "printing migration destination contents:"
-    ls -lah ${SITE_DIR}/default/files
-  fi
-
   # init mounted filesystems if not done or version updated (ECS Fargate EFS limitation forces this approach)
   if [[ "$(cat ${SITE_DIR}/.fs-done)" != "$DRUPAL_IMAGE_TAG" ]]; then
     echo "init_filesystems - initializing '${SITE_DIR}', '${THEME_DIR}', '${CORE_DIR}' & '${WWW_DIR}/resources' ..."
