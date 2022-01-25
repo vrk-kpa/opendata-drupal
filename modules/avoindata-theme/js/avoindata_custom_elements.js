b6c9b2f8b43f59369ddd9f5580b6b01d8486af1a
Pasi Laakso <pasi.laakso@gofore.com>
2022-01-25 16:07:42 +0200

    feature(AV-1575): Add content based scrollspy support for guide menu

--- modules/avoindata-theme/js/avoindata_custom_elements.js
+++ modules/avoindata-theme/js/avoindata_custom_elements.js
@@ -62,4 +62,55 @@ else {
     }
 
   }
+
+  Drupal.behaviors.guideMenuListener = {
+    attach: function (context) {
+      $('#guide-menu-column .nav__item.open .subnav', context).each(guideMenuInit);
+    }
+  }
+
+  /**
+   * Initializes interaction between guide menu and content
+   */
+  function guideMenuInit(index, subnavElement) {
+    // Make nav-item activation working also by scroll position
+    guideMenuListenScroll(subnavElement);
+  }
+
+  function guideMenuListenScroll(subnavElement) {
+    const $sections = $('.avoindata-section');
+    // Init trigger position on top of window
+    const triggerPosition = 0;
+    // Only add scroll listener when there is sections in page
+    if ($sections.length > 0) {
+      $('body').on('scroll', function () {
+        for (let i = 0; i < $sections.length; i++) {
+          const section = $sections[i];
+          const offsetTop = $(section).offset().top - parseInt($(section).css('marginTop'));
+          const offsetTopSectionBottom = offsetTop + $(section).outerHeight();
+
+          // As long as section is visible in screen, set it active in menu
+          if (offsetTop < triggerPosition  && triggerPosition <= offsetTopSectionBottom) {
+            let hash = '#' + $(section).attr('id');
+            guideMenuSetActiveSubnavItemByHash(subnavElement, hash);
+            break;
+          }
+        }
+      });
+    }
+  }
+
+  /**
+   * Sets active item in menu by given hash
+   */
+  function guideMenuSetActiveSubnavItemByHash(subnavElement, hash) {
+    $('.nav__item__link.active--subnav-link', subnavElement).removeClass('active--subnav-link');
+    $('a[href="' + hash + '"]', subnavElement).addClass('active--subnav-link');
+    var url = new URL(window.location);
+    url.hash = hash;
+
+    if (window.location.hash !== hash) {
+      history.replaceState({}, "", url);
+    }
+  }
 })(jQuery, Drupal);
