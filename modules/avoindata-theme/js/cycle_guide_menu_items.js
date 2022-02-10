31e35890bc82656582619904933f8858c0189f4e
Daniel Vainio <daniel.vainio@anders.com>
2022-02-10 16:47:51 +0200

    fix(AV-1578): Fixes for guide menu previous and next buttons
    Update the javscript to use the new selectors since the last
    update of the templates. Break the javascript code into separate
    classes.
    
    Get rid off the previous design and implement a div holding
    the next and previous buttons as links according to specs.
    
    Refs AV-1578

--- modules/avoindata-theme/js/cycle_guide_menu_items.js
+++ modules/avoindata-theme/js/cycle_guide_menu_items.js
@@ -1,145 +1,187 @@
 class MenuItem {
-    constructor(path, itemName) {
+    constructor(path, itemName, subnavItem, next, prev) {
         this.path = path;
         this.itemName = itemName;
+        this.subnavItem = subnavItem;
+        this.next = next; // Next will always be the next MAIN link (never subnav)
+        this.prev = prev; // Next will always be the previous MAIN link (never subnav)
     }
 }
 
-class CycleGuideMenuItem {
+class MenuUtils {
     constructor() {
-        this.menu = this.getMenu();
-        this.paths = this.getMenuPaths();
+        this.subnavSelector = 'subnav';
+        this.navItemSelector = 'nav__item';
+        this.navItemLinkSelector = 'nav__item__link';
+        this.navItemLinkContentSelector = 'nav__item__link__content';
 
-        this.currentPath = window.location.pathname;
-        this.nextBtn = document.getElementById('next-page-btn');
-        this.prevBtn = document.getElementById('previous-page-btn');
+        this.menuHeadingIds = [
+            'block-guidemenuen-menu',
+            'block-guidemenufi-menu',
+            'block-guidemenusv-menu'
+        ]
 
-        this.setBtnTexts();
-    }
-
-    attachBtnEvents() {
-        this.nextBtn.addEventListener('click', event  => { this.moveToNextPage() });
-        this.prevBtn.addEventListener('click', event  => { this.moveToPreviousPage() });
-    }
-
-    setBtnTexts() {
-        const currentPageIndex = this.getIndexOfCurrentPage();
-        this.setNextBtnText(currentPageIndex);
-        this.setPrevBtnText(currentPageIndex);
-    }
-
-    setNextBtnText(currentPageIndex) {
-        const nextString = this.nextBtn.innerHTML;
-
-        if (this.isFrontpage()) {
-            this.nextBtn.innerHTML = nextString + ': ' + this.paths[0].itemName;
-        } else {
-            if (currentPageIndex < this.paths.length - 1) {
-                let nextPageIndex = currentPageIndex + 1;
-                this.nextBtn.innerHTML = nextString + ': ' + this.paths[nextPageIndex].itemName;
-            }
-            else {
-                this.nextBtn.disabled = true;
-            }
-        }
-    }
-
-    setPrevBtnText(currentPageIndex) {
-        const prevString = this.prevBtn.innerHTML;
-
-        if (this.isFrontpage() || currentPageIndex == 0) {
-            this.prevBtn.disabled = true;
-        } else {
-            if (currentPageIndex > 0) {
-                let prevPageIndex = currentPageIndex - 1;
-                this.prevBtn.innerHTML = prevString + ': ' + this.paths[prevPageIndex].itemName;
-            }
-        }
-    }
-
-    getMenu() {
-        const menuLinkSelectors = [
+        this.menuLinkSelectors = [
             'block-guidemenuen',
             'block-guidemenufi',
             'block-guidemenusv'
         ]
 
-        for (let i = 0; i < menuLinkSelectors.length; i++) {
-            const selector = menuLinkSelectors[i];
+        this.menu = this.getMenu();
+    }
+
+    getMenu() {
+        for (let i = 0; i < this.menuLinkSelectors.length; i++) {
+            const selector = this.menuLinkSelectors[i];
             const menu = document.getElementById(selector);
             if (menu) {
                 return menu;
             }
         }
-
         return null;
     }
 
     getMenuPaths() {
-        const aTags = this.menu.getElementsByTagName('a');
         let paths = [];
 
-        for (const tag of aTags) {
-            let path = tag.getAttribute('href');
-            let itemName = tag.getElementsByClassName('link-content');
+        const headerLink = this.getHeaderAnchorLink();
+        if (headerLink) {
+            paths.push(headerLink);
+        }
+
+        const menuItems = this.menu.getElementsByClassName(this.navItemLinkSelector);
+        for (let item of menuItems) {
+            const subnavElement = this.isSubnavElement(item);
+            const path = item.getAttribute('href');
+            const itemName = item.getElementsByClassName(this.navItemLinkContentSelector);
+            const next = null;
+            const prev = null;
+
             if (itemName.length > 0) {
-                paths.push(new MenuItem(path, itemName[0].innerText));
+                paths.push(new MenuItem(path, itemName[0].innerText, subnavElement, next, prev));
             }
         }
 
-        return paths
+        // Set the next and prev pointers so that they're always pointing at the next or previous main items in the list.
+        for (let i = 0; i < paths.length; i++) {
+            if (i === 0) {
+                paths[i].next = this.getNextMainElementIndex(i, paths);
+            }
+
+            if (i === paths.length - 1) {
+                paths[i].prev = this.getPreviousMainElementIndex(i, paths);
+            }
+
+            if (i > 0 && i < paths.length) {
+                paths[i].prev = this.getPreviousMainElementIndex(i, paths);
+                paths[i].next = this.getNextMainElementIndex(i, paths);
+            }
+        }
+
+        return paths;
+    }
+
+    getPreviousMainElementIndex(index, paths) {
+        const currentItem = paths[index];
+        for (let i = index - 1; i >= 0; i--) {
+            if (paths[i].subnavItem === false) {
+                // If currentItem is a subNavitem, then the first previous main item that we encounter is the parent item.
+                // So we need to get the previous item from the parent in order for this to be correct.
+                if (currentItem.subnavItem === true && i > 0) {
+                    return i - 1;
+                }
+                return i;
+            }
+        }
+    }
+
+    getNextMainElementIndex(index, paths) {
+        for (let i = index + 1; i < paths.length; i++) {
+            if (paths[i].subnavItem === false) {
+                return i;
+            }
+        }
+    }
+
+    isSubnavElement(item) {
+        const subnavClass = '.' + this.subnavSelector;
+        return item.closest(subnavClass) !== null;
+    }
+
+    getHeaderAnchorLink() {
+        /**
+         * Get thhe header link since the header is a separate element and might contain a link.
+         * This should also be the first page of the guide page where the users lands when clicking
+         * the menu.
+         */
+        for (const path of this.menuHeadingIds) {
+            const link = document.getElementById(path);
+            if (link) {
+                const subnavElement = this.isSubnavElement(link);
+                let linkAnchors = link.getElementsByTagName('a');
+                if (linkAnchors.length > 0) {
+                    let href = linkAnchors[0].getAttribute('href');
+                    return new MenuItem(href, link.innerText, subnavElement);
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
+class CycleMenuItemsView {
+    constructor(menuUtils) {
+        this.currentPath = window.location.pathname;
+        this.nextBtn = document.getElementById('next-page-btn');
+        this.prevBtn = document.getElementById('previous-page-btn');
+        this.paths = menuUtils.getMenuPaths();
+
+        this.setAnchorLinks();
+    }
+
+    setAnchorLinks() {
+        const currentPageIndex = this.getIndexOfCurrentPage();
+
+        this.setNextAnchorLink(currentPageIndex);
+        this.setPrevAnchorLink(currentPageIndex);
+    }
+
+    setNextAnchorLink(currentPageIndex) {
+        const currentElement = this.paths[currentPageIndex];
+        if (currentElement.next === undefined) {
+            this.nextBtn.innerHTML = '';
+            this.nextBtn.href = '';
+            return;
+        }
+
+        const innerChevron = '<i class="far fa-chevron-right"></i>';
+        const nextMainElement = this.paths[currentElement.next];
+
+        this.nextBtn.innerHTML = nextMainElement.itemName + innerChevron;
+        this.nextBtn.href = nextMainElement.path;
+    }
+
+    setPrevAnchorLink(currentPageIndex) {
+        const currentElement = this.paths[currentPageIndex];
+        if (currentElement.prev === undefined) {
+            this.prevBtn.innerHTML = '';
+            this.prevBtn.href = '';
+            return;
+        }
+
+        const innerChevron = '<i class="far fa-chevron-left"></i>';
+        const prevMainElement = this.paths[currentElement.prev];
+
+        this.prevBtn.innerHTML = innerChevron + prevMainElement.itemName;
+        this.prevBtn.href = prevMainElement.path;
     }
 
     getIndexOfCurrentPage() {
         return this.paths.map(function (e) { return e.path; }).indexOf(this.currentPath);
     }
-
-    moveToNextPage() {
-        if (this.paths === []) { return; }
-
-        if (this.isFrontpage()) {
-            window.location = this.paths[0].path;
-        } else {
-            const index = this.getIndexOfCurrentPage();
-            const nextPage = index + 1;
-            if (nextPage < this.paths.length) {
-                window.location = this.paths[nextPage].path;
-            }
-        }
-    }
-
-    moveToPreviousPage() {
-        if (this.paths === []) { return; }
-
-        if (this.isFrontpage()) {
-            return;
-        } else {
-            const index = this.getIndexOfCurrentPage();
-            const prevPage = index - 1;
-            if (prevPage >= 0) {
-                window.location = this.paths[prevPage].path;
-            }
-        }
-    }
-
-    isFrontpage() {
-        const frontpagePaths = [
-            '/fi/opas/oppaan-etusivu',
-            '/sv/guide/guide-startsidan',
-            '/en/guide/guide-frontpage'
-        ]
-
-        for (const path of frontpagePaths) {
-            if (path === this.currentPath) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    const cycle = new CycleGuideMenuItem();
-    cycle.attachBtnEvents();
+    new CycleMenuItemsView(new MenuUtils());
 });
